f985936978d5970b6aa649ed29ff4a1b8981973d
Mert Ömer BAKAR <95905493+mertomer@users.noreply.github.com>
2024-06-03 23:59:34 +0300

    first commit

---
+++ template.js
@@ -0,0 +1,87 @@
+// Dark mode 
+function dark_light() {
+    let darkData = document.body.classList.toggle("dark_mode_css")
+}
+
+//Search search_id
+$(function () {
+    const searchData = ["Adana", "Bursa", "Diyarbakir", "Elazığ", "Giresun", "Kastamonu", "Malatya", "Niğde", "Van"];
+    $("#search_id").autocomplete({
+        source: searchData
+    });
+})
+
+// Register 
+// Eğer inputlar dolu ise validation yazı silinecek
+$(function () {
+    $("#registerSubmit").click(function () {
+        let nickname, email, passwd;
+        //nickname
+        nickname = $("#nickname").val();
+        nickname = jQuery.trim(nickname);
+
+        //email
+        email = $("#email").val();
+        email = jQuery.trim(email);
+
+        //nickname
+        passwd = $("#passwd").val();
+        passwd = jQuery.trim(passwd);
+
+        //validation nickname
+        if (nickname == "") {
+            $("#nicknameValidation").html(`<b>Nickname boş geçilemez</b>`)
+        }
+
+        //passwdValidation
+        if (passwd == "") {
+            $("#passwdValidation").html(`<b>passwd boş geçilemez</b>`)
+        }
+
+        //emailValidation
+        if (email == "") {
+            $("#emailValidation").html(`<b>email boş geçilemez</b>`)
+        } else if (validateEmail(email) == false) {
+            $("#emailValidation").html(`<b>email uygun formatta yazmadınız</b>`)
+        }
+
+        //arrow function ile anonymous function arasındaki fark
+        function validateEmail (email)  {
+            const regex =/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return regex.test(email);
+        }
+
+    });
+})
+
+
+// LOGIN ODEV
+
+//BackToTop 
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $("#back_top").fadeIn('slow');
+    } else {
+        $("#back_top").fadeOut('slow');
+    }
+});
+$("#back_top").click(function () {
+    $('html,body').animate({ scrollTop: 0 }, 100, 'easeInOutExpo');
+    return false;
+});
+
+
+
+//now date 
+let now = () => {
+    //HTML
+    document.getElementById("now_date").innerHTML = new Date().getFullYear();
+
+    // CSS
+    $("#now_date").css("color", "red");
+    const cssDate = {
+        "box-shadow": "1px 1px 3px 1px rgba(255,255,255,0.1)",
+    }
+    $("#now_date").css(cssDate);
+}
+now();
